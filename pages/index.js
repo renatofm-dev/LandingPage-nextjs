03ec97226eb637191ec21d38d9faec8440e99161
Renato Martins <rmartins6694@gmail.com>
2023-02-26 20:19:27 -0300

    add title

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
 import Link from "next/link"
 
+
+function Title ({ children }) {
+  return (
+  <>
+    <h1>
+    {children}
+  </h1>
+
+  <style jsx>
+    {`
+      h1 {
+        color: red;
+      }
+    `}
+  </style>
+  </>
+  )
+
+}
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Bom dia mundo maravilhoso</h1>
+      <Title>Bom dia mundo maravilhoso</Title>
       <Link href="/faq">
         FAQ
       </Link>
