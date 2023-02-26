7325d807cf84aabfaae07a39ec29b73706b4062d
Renato Martins <rmartins6694@gmail.com>
2023-02-26 09:57:01 -0300

    first commit

---
+++ pages/faq.js
@@ -0,0 +1,9 @@
+export default function FAQPage() {
+  return (
+    <div>
+      <h1>
+        FAQ - quais os seus questionamentos
+      </h1>
+    </div>
+  )
+} 
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Bom dia mundo maravilhoso</h1>
+      <a href="/faq">FAQ</a>
+    </div>
+  )
+}
